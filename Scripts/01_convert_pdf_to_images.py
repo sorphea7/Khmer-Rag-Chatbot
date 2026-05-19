a45176c0ea26dc590e61c3354052b831fa8a0338
sorphea7 <sorpheaponh205@gmail.com>
2026-05-19 19:29:04 +0700

    Convert all the pdfs to images by looping all the pdf files

--- Scripts/01_convert_pdf_to_images.py
+++ Scripts/01_convert_pdf_to_images.py
@@ -1,14 +1,39 @@
 from pdf2image import convert_from_path
 from pathlib import Path
 
-pdf_path = "data/source_documents/laws/law_01.pdf"
+# Root source folder
+source_root = Path("data/source_documents")
 
-output_dir = Path("data/page_images/laws/law_01")
-output_dir.mkdir(parents=True, exist_ok=True)
+# Root output folder
+output_root = Path("data/page_images")
 
-pages = convert_from_path(pdf_path, dpi=300)
+# Find all PDFs recursively
+pdf_files = source_root.rglob("*.pdf")
 
-for i, page in enumerate(pages):
-    page.save(output_dir / f"page_{i+1:03}.png", "PNG")
+for pdf_path in pdf_files:
 
-print("PDF converted successfully.")
+    # Example:
+    # laws/law_01.pdf
+    category = pdf_path.parent.name
+    pdf_name = pdf_path.stem
+
+    # Output folder
+    output_dir = output_root / category / pdf_name
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    print(f"\nProcessing: {pdf_path}")
+
+    # Convert PDF pages to images
+    pages = convert_from_path(pdf_path, dpi=300)
+
+    # Save pages
+    for i, page in enumerate(pages):
+
+        page_path = output_dir / f"page_{i+1:03}.png"
+
+        page.save(page_path, "PNG")
+
+    print(f"Completed: {pdf_name}")
+
+print("\nAll PDFs converted successfully.")
